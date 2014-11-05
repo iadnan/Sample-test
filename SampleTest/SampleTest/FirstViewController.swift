324a7b676fab05d22cce43b2971f8d6cb528fdfc
Ibrahim Adnan <ibrahim.adnan@lge.com>
2014-11-05 13:35:20 -0800

    Adding sample proj

---
+++ SampleTest/SampleTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SampleTest
+//
+//  Created by Ibrahim Adnan on 11/5/14.
+//  Copyright (c) 2014 Ibrahim Adnan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
